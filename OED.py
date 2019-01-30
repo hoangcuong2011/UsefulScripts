0e06535c42d97675278f41f51b1b07df99b5d25b
Hoang Cuong <hoangcuong2011@gmail.com>
2019-01-30 21:16:10 +0700

    Update OED.py

--- OED.py
+++ OED.py
@@ -8,36 +8,32 @@ https://rkevingibson.github.io/blog/neural-networks-as-ordinary-differential-equ
 https://github.com/jason71995/Keras_ODENet/blob/master/mnist_odenet.py
 https://www.reddit.com/r/MachineLearning/comments/a65v5r/neural_ordinary_differential_equations_pdf/
 """
-
 import tensorflow as tf
 import keras.backend as K
 import keras
 from keras.datasets import mnist
 from keras.models import Model
 from keras.layers import Conv2D, Dense, Flatten, Input, MaxPooling2D
+from keras.layers import Layer
 
-def set_gpu_config(device = "0",fraction=0.25):
-	config = tf.ConfigProto()
-	config.gpu_options.per_process_gpu_memory_fraction = fraction
-	config.gpu_options.visible_device_list = device
-	K.set_session(tf.Session(config=config))
-
-class ODEBlock(Model):
+class ODEBlock(Layer):
 	def __init__(self, filters, kernel_size, **kwargs):
 		self.filters = filters
 		self.kernel_size = kernel_size
 		super(ODEBlock, self).__init__(**kwargs)
 
 	def build(self, input_shape):
-		self.Conv2DLayer = Conv2D(self.filters, self.kernel_size, padding="same", activation="relu")
+		self.Conv2DLayer1 = Conv2D(self.filters, self.kernel_size, padding="same", activation="relu")
+		self.Conv2DLayer2 = Conv2D(self.filters, self.kernel_size, padding="same", activation="relu")
 		super(ODEBlock, self).build(input_shape)
 
 	def block(self, x, t):
-		return self.Conv2DLayer(x)
+		return self.Conv2DLayer2(self.Conv2DLayer1(x))
 
 	def call(self,x):
-		t = K.variable([0,1.0],dtype="float32")
+		t = K.constant([0,1.0],dtype="float32")
 		return tf.contrib.integrate.odeint(self.block, x, t, rtol=1e-3, atol=1e-3)[1]
+
 	def compute_output_shape(self, input_shape):
 		print(input_shape)
 		return (input_shape[0], input_shape[1], input_shape[2], input_shape[3])
@@ -49,13 +45,9 @@ def build_model(input_shape, num_classes):
 	y = Conv2D(64, (3, 3), activation='relu')(y)
 	y = MaxPooling2D((2,2))(y)
 	y = ODEBlock(64, (3, 3))(y)
-	y = ODEBlock(64, (3, 3))(y)
 	y = Flatten()(y)
 	y = Dense(num_classes, activation='softmax')(y)
 	return Model(x,y)
-
-
-set_gpu_config("0",0.25)
 
 batch_size = 128
 num_classes = 10
